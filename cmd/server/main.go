02e3c4658d412e509add2ee14300c7a9a1464f1d
DimRev <dimrev444@gmail.com>
2024-05-17 14:21:41 +0300

    Add pubsub int-pkg, publish json to rabbitMQ server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,27 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
+	"syscall"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	conStr := "amqp://guest:guest@localhost:5672/"
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 
-	fmt.Println("Starting Peril server...")
-	conn, err := amqp.Dial(conStr)
+	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Could not connect to RabbitMQ: %v", err)
 	}
-
 	defer conn.Close()
-
 	fmt.Println("Peril game server connected to RabbitMQ!")
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Could not setup a channel with RabbitMQ: %v", err)
+	}
 
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatalf("Could not publish: %v", err)
+	}
+
+	// Wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+	<-signalChan
+	fmt.Println("\nReceived interrupt signal. Exiting...")
 }
