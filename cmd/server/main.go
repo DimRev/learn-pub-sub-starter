94055fb722a44448650caa34f5e3e4425fdfc353
DimRev <dimrev444@gmail.com>
2024-05-17 12:59:09 +0300

    Aconn to local rabbitmq and wait for Interrupt sig

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	conStr := "amqp://guest:guest@localhost:5672/"
+
 	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(conStr)
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
 }
